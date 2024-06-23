5902d9a0464dfc336647ee6ce612ebe76fecd348
leongaultier <leongaultierr@gmail.com>
2024-06-23 16:51:16 +0500

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  note-taking-app
+//
+//  Created by Léon Gaultier on 23.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ note_taking_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  note_taking_appApp.swift
+//  note-taking-app
+//
+//  Created by Léon Gaultier on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct note_taking_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
